5b66a6a5ac46d2d851c81113926a3f7879cb1785
KeishiS <sando.keishi.sp@alumni.tsukuba.ac.jp>
2025-12-04 07:04:55 +0900

    added feature flags

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
+#[cfg(feature = "python")]
 use pyo3::pymodule;
 
 fn sum_as_str(a: i64, b: i64) -> String {
     (a + b).to_string()
 }
 
+#[cfg(feature = "python")]
 #[pymodule]
 #[pyo3(name = "_keishis_sandbox")]
 mod keishis_sandbox {
