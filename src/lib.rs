584829da97db5629b431e51b291750d5a6aeaff1
KeishiS <sando.keishi.sp@alumni.tsukuba.ac.jp>
2025-12-04 07:33:32 +0900

    updated

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,26 @@
 #[cfg(feature = "python")]
 use pyo3::pymodule;
 
-fn sum_as_str(a: i64, b: i64) -> String {
-    (a + b).to_string()
+fn _mysum(a: i64, b: i64) -> i64 {
+    a + b
 }
 
 #[cfg(feature = "python")]
 #[pymodule]
 #[pyo3(name = "_keishis_sandbox")]
 mod keishis_sandbox {
-    use super::sum_as_str as _sum_as_str;
+    use super::_mysum;
     use pyo3::prelude::PyResult;
     use pyo3::pyfunction;
 
     #[pyfunction]
-    fn sum_as_str(a: i64, b: i64) -> PyResult<String> {
-        Ok(_sum_as_str(a, b))
+    fn mysum(a: i64, b: i64) -> PyResult<i64> {
+        Ok(_mysum(a, b))
     }
+}
+
+#[cfg(feature = "julia")]
+#[unsafe(no_mangle)]
+pub extern "C" fn mysum(a: i64, b: i64) -> i64 {
+    _mysum(a, b)
 }
